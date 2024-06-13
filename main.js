a1813a8b192bdc8eb9efd306dd93dcca708fbd08
dohaghaly <dohagaly995@gmail.com>
2024-06-13 19:40:13 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+
+
+
+ for(let i=1;i<=12;i++){
+    console.log(2*i)
+ }
+ for(let i=1;i<=12;i++){
+    console.log(3*i)
+ }
+ for(let i=1;i<=12;i++){
+    console.log(4*i)
+ }
+
+ for(let i=1;i<=12;i++){
+    console.log(5*i)
+ }
+ for(let i=1;i<=12;i++){
+    console.log(6*i)
+ }
+ for(let i=1;i<=12;i++){
+    console.log(7*i)
+ }
+ for(let i=1;i<=12;i++){
+    console.log(8*i)
+ }
+ for(let i=1;i<=12;i++){
+    console.log(9*i)
+ }
+ for(let i=1;i<=12;i++){
+    console.log(10*i)
+ }
+ for(let i=1;i<=12;i++){
+    console.log(11*i)
+ }
+ for(let i=1;i<=12;i++){
+    console.log(12*i)
+ }
+ 
